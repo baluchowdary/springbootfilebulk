d76c838eb51f187f90661d831d49ce7191657687
baluchowdary <bhardwaja.kollu@gmail.com>
2026-02-07 21:48:38 -0600

    Initial commit

---
+++ src/main/java/com/kollu/springbootfilebulk/SpringbootfilebulkApplication.java
@@ -0,0 +1,13 @@
+package com.kollu.springbootfilebulk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfilebulkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootfilebulkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kollu/springbootfilebulk/SpringbootfilebuilkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kollu.springbootfilebulk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootfilebulkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

dd8b9ca26c097d574ce30fb354fcf699f46b48ee
baluchowdary <bhardwaja.kollu@gmail.com>
2026-02-10 01:12:24 -0600

    Commented code and added comments for MongoTransactionManager , It not supported standalone systems
    Not able to create batch_job_execution_context Meta table due to unsupported MongoTransactionManager.
    
    When MongoTransactionManager support, we can capture -> Stores the serialized state of the job, including the exact line number where the reader stopped, based on last line number we can find Persistent Checkpoints.

--- src/main/java/com/kollu/springbootfilebulk/config/MongoBatchConfig.java
+++ src/main/java/com/kollu/springbootfilebulk/config/MongoBatchConfig.java
@@ -23,81 +23,107 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
+//public class MongoBatchConfig extends DefaultBatchConfiguration { kollu-This line for - MongoTransactionManager
 public class MongoBatchConfig {
 
-	//commented because no replicaSet for MongoDB
+	// commented because no replicaSet for MongoDB - kollu-Enable below code for MongoTransactionManager
 	/*
 	 * @Bean public MongoTransactionManager transactionManager(MongoDatabaseFactory
 	 * dbFactory) { return new MongoTransactionManager(dbFactory); }
 	 */
-	
-	//Manually creating sequence no, MongoDB doesn't create auto increment secuence numbers
+
+	// Manually creating sequence no, MongoDB doesn't create auto increment secuence
+	// numbers
 	@Bean
 	public CommandLineRunner initBatchSequences(MongoOperations mongoOperations) {
-	    return args -> {
-	        String[] sequences = {
-	            "BATCH_JOB_INSTANCE_SEQ", 
-	            "BATCH_JOB_EXECUTION_SEQ", 
-	            "BATCH_STEP_EXECUTION_SEQ"
-	        };
-	        
-	        for (String seq : sequences) {
-	            Query query = new Query(Criteria.where("_id").is(seq));
-	            if (!mongoOperations.exists(query, "BATCH_SEQUENCES")) {
-	                Document doc = new Document();
-	                doc.put("_id", seq);
-	                doc.put("count", 0L);
-	                mongoOperations.insert(doc, "BATCH_SEQUENCES");
-	            }
-	        }
-	    };
+		return args -> {
+			String[] sequences = { "BATCH_JOB_INSTANCE_SEQ", "BATCH_JOB_EXECUTION_SEQ", "BATCH_STEP_EXECUTION_SEQ" };
+
+			for (String seq : sequences) {
+				Query query = new Query(Criteria.where("_id").is(seq));
+				if (!mongoOperations.exists(query, "BATCH_SEQUENCES")) {
+					Document doc = new Document();
+					doc.put("_id", seq);
+					doc.put("count", 0L);
+					mongoOperations.insert(doc, "BATCH_SEQUENCES");
+				}
+			}
+		};
 	}
 	
-	//Batch by default won't accept dot
-	@Bean
-    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory factory, MongoMappingContext context) {
-        DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
-        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, context);
-        
-        // This replaces dots ( . ) with double underscores ( __ ) in Map keys
-        converter.setMapKeyDotReplacement("__"); 
-        
-        return converter;
-    }
+	//commented below for MongoTransactionManager -kollu enable below line
+	//@Autowired
+    //private MongoTemplate mongoTemplate;
 	
-	//Updated ResourcelessTransactionManager, we are working single node region
+
+	// Updated ResourcelessTransactionManager, we are working single node region
 	@Bean
 	public PlatformTransactionManager transactionManager() {
-	    return new ResourcelessTransactionManager();
+		// This manager is designed for systems that don't support 
+	    // traditional transactions (like standalone MongoDB)
+		return new ResourcelessTransactionManager();
+	}
+ 
+	// Batch by default won't accept dot
+	@Bean
+	public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory factory, MongoMappingContext context) {
+		DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
+		MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, context);
+
+		// This replaces dots ( . ) with double underscores ( __ ) in Map keys
+		converter.setMapKeyDotReplacement("__");
+
+		return converter;
 	}
 
-    @Bean
-    public JobRepository jobRepository(MongoTemplate mongoTemplate, PlatformTransactionManager transactionManager) throws Exception {
-        MongoJobRepositoryFactoryBean factory = new MongoJobRepositoryFactoryBean();
-        factory.setMongoOperations(mongoTemplate);
-        factory.setTransactionManager(transactionManager);
-        
-        // Optional: Custom collection prefix (default is none, unlike JDBC's BATCH_)
-       // factory.setCollectionPrefix("MONGO_META_"); 
-        
-        factory.afterPropertiesSet();
-        return factory.getObject();
-    }
+	@Bean
+	public JobRepository jobRepository(MongoTemplate mongoTemplate, PlatformTransactionManager transactionManager)
+			throws Exception {
+		MongoJobRepositoryFactoryBean factory = new MongoJobRepositoryFactoryBean();
+		factory.setMongoOperations(mongoTemplate);
+		factory.setTransactionManager(transactionManager);
 
-    @Bean
-    public JobLauncher jobLauncher(JobRepository jobRepository) throws Exception {
-        TaskExecutorJobLauncher jobLauncher = new TaskExecutorJobLauncher();
-        jobLauncher.setJobRepository(jobRepository);
-        jobLauncher.afterPropertiesSet();
-        return jobLauncher;
-    }
+		// Optional: Custom collection prefix (default is none, unlike JDBC's BATCH_)
+		// factory.setCollectionPrefix("MONGO_META_");
+
+		factory.afterPropertiesSet();
+		return factory.getObject();
+	}
+
+	@Bean
+	public JobLauncher jobLauncher(JobRepository jobRepository) {
+
+		TaskExecutorJobLauncher jobLauncher = new TaskExecutorJobLauncher();
+		try {
+			jobLauncher.setJobRepository(jobRepository);
+			jobLauncher.afterPropertiesSet();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return jobLauncher;
+	}
+
+	@Bean
+	public JobExplorer jobExplorer(MongoTemplate mongoTemplate, PlatformTransactionManager transactionManager)
+			throws Exception {
+		MongoJobExplorerFactoryBean factory = new MongoJobExplorerFactoryBean();
+		factory.setMongoOperations(mongoTemplate);
+		factory.setTransactionManager(transactionManager);
+		factory.afterPropertiesSet();
+		return factory.getObject();
+	}
+	
+	//Enable below code for MongoTransactionManager -kollu
     
-    @Bean
-    public JobExplorer jobExplorer(MongoTemplate mongoTemplate, PlatformTransactionManager transactionManager) throws Exception {
-        MongoJobExplorerFactoryBean factory = new MongoJobExplorerFactoryBean();
-        factory.setMongoOperations(mongoTemplate);
-        factory.setTransactionManager(transactionManager);
-        factory.afterPropertiesSet();
-        return factory.getObject();
-    }
+//	@Override
+//    protected JobRepository createJobRepository() throws Exception {
+//        // The built-in MongoJobFactoryBean is often preferred for 
+//        // managing complex initialization automatically.
+//        MongoJobRepositoryFactoryBean factoryBean = new MongoJobRepositoryFactoryBean();
+//        factoryBean.setMongoOperations(mongoTemplate);
+//        factoryBean.setTransactionManager(transactionManager(mongoTemplate.getMongoDatabaseFactory()));
+//        factoryBean.afterPropertiesSet();
+//        return factoryBean.getObject();
+//    }
+    
 }
